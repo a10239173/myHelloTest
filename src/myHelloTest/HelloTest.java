f96a669e6b85206b8dbf984f6ea18bdeeb06eeef
Administrator <a10239173@163.com>
2020-02-21 15:18:08 +0800

    11

--- src/myHelloTest/HelloTest.java
+++ src/myHelloTest/HelloTest.java
@@ -3,6 +3,6 @@ package myHelloTest;
 public class HelloTest {
 
 	public static void main(String[] args) {
-		System.out.println("my hellotest 2020!¸ÄÒ»¸Ä");
+		System.out.println("my hellotest 2020!2022");
 	}
 }
