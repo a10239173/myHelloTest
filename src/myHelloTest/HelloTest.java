0f4ec33f965c2e3bf73162caf254bc666bb3b05c
Administrator <a10239173@163.com>
2020-02-21 15:15:40 +0800

    修改

--- src/myHelloTest/HelloTest.java
+++ src/myHelloTest/HelloTest.java
@@ -3,6 +3,6 @@ package myHelloTest;
 public class HelloTest {
 
 	public static void main(String[] args) {
-		System.out.println("my hellotest 2020!");
+		System.out.println("my hellotest 2020!¸ÄÒ»¸Ä");
 	}
 }
